aafc1ad1193d6a11881fcd5969fc4ce3f5063043
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-23 15:47:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(source_array)
+  newArray = []
+  i = 0 
+  while i < source_array.length do
+    newArray.push(source_array[i] * -1)
+  end
+  yield
+end
+
+map() do
+  |innards|
+  return new_array
+end
+
+
